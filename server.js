eebaef4a8a1060b35ea515c821ac18eaa327c139
Hristo Tanev <hristotanev7@gmail.com>
2016-08-21 18:34:39 +0100

    added the server

---
+++ server.js
@@ -0,0 +1,16 @@
+let express=require('express');
+let webpack=require('webpack');
+let webpackDevMiddleware=require('webpack-dev-middleware');
+let webpackHotMiddleware=require('webpack-hot-middleware');
+let config=require('./webpack.config');
+
+let app=new express();
+let compiler=webpack(config);
+app.use(webpackDevMiddleware(compiler));
+app.use(webpackHotMiddleware(compiler));
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname+'');
+});
+
+app.listen(3000);
